0b6af7582d061e7a6ceda0e5f4246501d8b96fa0
michelleroos <hello.michelleroos@gmail.com>
2021-04-19 08:59:33 -0400

    Continued on clock

--- clock.js
+++ clock.js
@@ -14,6 +14,36 @@ class Clock {
         console.log(time);
     }
 
+    _tick() {
+        let that = this;
+
+        setInterval(function () {
+            if (that.seconds === 59) {
+                that.seconds = 0;
+
+                if (that.minutes === 59) {
+                    that.minutes = 0;
+
+                    if (that.hours === 23) {
+                        that.hours = 0;
+                    } else {
+                        that.hours += 1;
+                    }
+
+                } else {
+                    that.minutes += 1;
+                }
+
+            } else {
+                that.seconds += 1;
+            }
+
+            that.printTime.call(that);
+        }, 1000);
+
+
+    }
+
 }
 
 // const clock = new Clock();
